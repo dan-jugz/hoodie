665b32fb4f98136f4fc7317081a6d72422223bc8
daniel njuguna <njugunadaniel364@gmail.com>
2019-10-28 15:39:38 +0300

    setup the neigborhood model

--- hood/models.py
+++ hood/models.py
@@ -7,3 +7,16 @@ class Location(models.Model):
     def __str__(self):
         return self.name
 
+class NeighbourHood(models.Model):
+    name = models.CharField(max_length = 30)
+    location = models.ForeignKey(Location, on_delete=models.CASCADE)
+    occupants = models.IntegerField(null=True, default=0)
+
+    def __str__(self):
+        return self.name
+
+    def create_neighborhood(self):
+        self.save()
+
+    def delete_neighborhood(self):
+        self.delete()
