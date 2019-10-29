907d174c8f6f2204616c24d0d36d483734cc2615
daniel njuguna <njugunadaniel364@gmail.com>
2019-10-29 14:22:06 +0300

    setup the route and templates for the user profile

--- hood/urls.py
+++ hood/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^business$', views.business, name='business'),
+    url(r'^profile/(?P<username>\w{0,50})$', views.profile, name='profile'),
 ]
