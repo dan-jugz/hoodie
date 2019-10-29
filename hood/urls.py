e3d6c474482a06dbecd46c05097f7364ded206d9
daniel njuguna <njugunadaniel364@gmail.com>
2019-10-29 14:27:21 +0300

    add a template and a route to the posts

--- hood/urls.py
+++ hood/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     url(r'^business$', views.business, name='business'),
     url(r'^profile/(?P<username>\w{0,50})$', views.profile, name='profile'),
     url(r'^edit_profile/(?P<username>\w{0,50})$', views.edit_profile,name='edit_profile'),
-]
+    url(r'^post/(\d+)$', views.post, name='post'),
+    ]
