524fc95353daae21c24d7c4e1fa5b6a8cf96af02
daniel njuguna <njugunadaniel364@gmail.com>
2019-10-29 11:24:23 +0300

    create a user model connected to a  neigborhood

--- hood/models.py
+++ hood/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Location(models.Model):
@@ -20,3 +21,14 @@ class NeighbourHood(models.Model):
 
     def delete_neighborhood(self):
         self.delete()
+
+
+class UserProfile(models.Model):
+    name = models.CharField(max_length = 50)
+    neighbourhood = models.ForeignKey(NeighbourHood, on_delete=models.CASCADE, null=True)
+    bio = models.TextField(null=True)
+    email = models.EmailField(max_length = 60, null=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
