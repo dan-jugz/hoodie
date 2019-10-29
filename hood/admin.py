1d1baf5aeaf2594e3af378191ff8f1e8c216c04a
daniel njuguna <njugunadaniel364@gmail.com>
2019-10-29 15:40:01 +0300

    register the moodels in the admin interface

--- hood/admin.py
+++ hood/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import NeighbourHood,UserProfile,Business,Location,Post
 
 # Register your models here.
+admin.site.register(NeighbourHood)
+admin.site.register(UserProfile)
+admin.site.register(Business)
+admin.site.register(Location)
+admin.site.register(Post)
