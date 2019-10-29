6500c5811fc65ee9fb5426a2d38d19aea1a9f88b
daniel njuguna <njugunadaniel364@gmail.com>
2019-10-29 12:32:12 +0300

    require the user to login to access the various pages and load the context of the businesses and homepage

--- hood/views.py
+++ hood/views.py
@@ -1,30 +1,47 @@
 from django.shortcuts import render
 from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
+from .models import Business,UserProfile,NeighbourHood,Post
 
 # Create your views here.
+@login_required(login_url='/accounts/login')
 def index(request):
     current_user = request.user
-    return render(request,'index.html', locals())
+    profile = UserProfile.objects.get(user = current_user)
+
+    context = {
+        "profile":profile,
+    }
+    return render(request,'index.html', context)
 
 
+@login_required(login_url='/accounts/login')
 def post(request,id):
     post = Post.objects.get(id = id)
     return render(request, 'post.html', locals())
 
 
+@login_required(login_url='/accounts/login')
 def business(request):
     profile = UserProfile.objects.get(user = request.user)
-    return render(request, 'business.html',locals())
+    businesses = Business.objects.filter(neighbourhood = profile.neighbourhood)
+    context = {
+        "businesses":businesses
+    }
+    return render(request, 'business.html',context)
 
 
+@login_required(login_url='/accounts/login')
 def new_business(request):
     return render(request,'new_business.html',locals)
 
 
+@login_required(login_url='/accounts/login')
 def profile(request):
     return render(request,'profile.html',locals())
 
 
+@login_required(login_url='/accounts/login')
 def edit_profile(request):
     return render(request,'edit_profile.html',locals())
 
