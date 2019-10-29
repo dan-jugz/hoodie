bbe02ef3a9c3bbd2c1a1e3ae91a233665a5924cd
daniel njuguna <njugunadaniel364@gmail.com>
2019-10-29 14:25:19 +0300

    add a route and template to use for when the user is editing their profile

--- hood/urls.py
+++ hood/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^business$', views.business, name='business'),
     url(r'^profile/(?P<username>\w{0,50})$', views.profile, name='profile'),
+    url(r'^edit_profile/(?P<username>\w{0,50})$', views.edit_profile,name='edit_profile'),
 ]
