8d69f2a09f4d7822d9123e3def5cc5683e740b21
daniel njuguna <njugunadaniel364@gmail.com>
2019-10-29 12:43:23 +0300

    add a profileform

---
+++ hood/forms.py
@@ -0,0 +1,9 @@
+from django import forms
+from .models import UserProfile,Business,NeighbourHood,Post
+
+class ProfileForm(forms.ModelForm):
+    class Meta:
+        model=UserProfile
+        exclude = ['user']
+
+        
