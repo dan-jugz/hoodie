15fe172e4806ddcdfe700cc4d9b0bc2ad7435651
daniel njuguna <njugunadaniel364@gmail.com>
2019-10-29 14:30:12 +0300

    add a route to take when writing a new post and the template to write it in

--- hood/urls.py
+++ hood/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     url(r'^profile/(?P<username>\w{0,50})$', views.profile, name='profile'),
     url(r'^edit_profile/(?P<username>\w{0,50})$', views.edit_profile,name='edit_profile'),
     url(r'^post/(\d+)$', views.post, name='post'),
+    url(r'^new_post$', views.new_post, name='new_post'),
     ]
